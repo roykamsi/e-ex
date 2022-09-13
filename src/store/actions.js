b658e28b939fafe9474e004d65e6d64369719aed
roykamsi <kamsi.rino@gmail.com>
2022-09-13 16:48:42 +0200

    Securify fix

--- src/store/actions.js
+++ src/store/actions.js
@@ -114,7 +114,7 @@ export default {
   async signup({ commit, state }, payload) {
     await axios
       .post(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${config.API_KEY}git`,
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${config.API_KEY}`,
         {
           email: payload.email,
           password: payload.password,
